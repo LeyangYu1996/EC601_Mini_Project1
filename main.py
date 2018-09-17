1946b430b10bc867c1002743ff0914c801183a90
LeyangYu1996 <43018204+LeyangYu1996@users.noreply.github.com>
2018-09-17 09:38:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -115,5 +115,5 @@ if __name__ == '__main__':
             Delect_Files()
             print('Mission Complete')
     else:
-        print('Error: no pictures found in this twitter account')
+        print('Error: no pictures downloaded from this twitter account')
 
